beb3fbf9cdb66eb2dfc4f742565ae7c3f51c74f9
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2023-04-19 18:42:06 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,8 @@ export default function Home() {
             />
             <button className="bg-red-600 px-9 py-2 rounded">Zoek nu</button>
           </div>
-          {/* SEARCHBAR */}
+          {/* HEADER */}
+          {/* SEARCHBAR SIDE BAR */}
         </div>
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
           <div className="w-1/5">
@@ -56,9 +57,51 @@ export default function Home() {
               <p className="font-light text-reg text-black">Frans</p>
               <p className="font-light text-reg text-black">Chinees</p>
             </div>
+            <div className="mt-3 pb-4">
+              <h1 className="mb-2">Prijs</h1>
+              <div className="flex">
+                <button className="border w-full text-reg rounded-l p-2 text-black ">
+                  $
+                </button>
+                <button className="border-r border-t border-b w-full text-reg p-2 text-black ">
+                  $$
+                </button>
+                <button className="border-r border-t border-b w-full text-reg p-2 text-black rounded-r ">
+                  $$$
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* SEARCH SIDE BAR */}
+          <div className="w-5/6">
+            {/* RESTAURANT CARD */}
+            <div className="border-b flex pb-5">
+              <img
+                src="https://media-cdn.tripadvisor.com/media/photo-p/12/3c/87/f6/de-muizelmolen.jpg"
+                alt=""
+                className="w-44 rounded"
+              />
+              <div className="pl-5">
+                <h2 className="text-3xl text-black">De Muizelmolen</h2>
+                <div className="flex items-start">
+                  <div className="flex mb-2 text-black">*****</div>
+                  <p className="ml-2 text-sm text-black">Perfect</p>
+                </div>
+                <div className="mb-9">
+                  <div className="font-light flex text-reg">
+                    <p className="mr-4 text-black">$$$</p>
+                    <p className="mr-4 text-black">Mexicaans</p>
+                    <p className="mr-4 text-black">Hulste</p>
+                  </div>
+                </div>
+                <div className="text-red-600">
+                  <a href="">Meer informatie</a>
+                </div>
+              </div>
+            </div>
+            {/* RESTAURANT CARD */}
           </div>
         </div>
-        {/* HEADER */}
       </main>
     </main>
   );
