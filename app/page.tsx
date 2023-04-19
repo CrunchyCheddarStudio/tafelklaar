fbda1d5cb72b32d5b3f7eed6ae8c4bda632b14b2
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2023-04-19 16:18:58 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,14 +82,70 @@ export default function Home() {
                 5 Foto's
               </h1>
               <div className="flex flex-wrap">
-                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/10/11/73/dd/photo0jpg.jpg" alt="" />
-                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/0b/d2/d6/dc/photo1jpg.jpg" alt="" />
-                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/68/t-populierenhof.jpg" alt="" />
-                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-l/0f/5b/1d/72/koude-visschotel-en-schotel.jpg" alt="" />
-                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/66/t-populierenhof.jpg" alt="" />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://media-cdn.tripadvisor.com/media/photo-s/10/11/73/dd/photo0jpg.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://media-cdn.tripadvisor.com/media/photo-s/0b/d2/d6/dc/photo1jpg.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/68/t-populierenhof.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://media-cdn.tripadvisor.com/media/photo-l/0f/5b/1d/72/koude-visschotel-en-schotel.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/66/t-populierenhof.jpg"
+                  alt=""
+                />
               </div>
             </div>
             {/* IMAGES */}
+            {/* REVIEWS */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5 text-black">
+                Wat vinden 100 personen van dit restaurant
+              </h1>
+              <div>
+                {/* REVIEW CARD */}
+                <div className="border-b pb-7 mb-7">
+                  <div className="flex">
+                    <div className="w-1/6 flex flex-col items-center">
+                      <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                        <h2 className="text-white text-2xl">MJ</h2>
+                      </div>
+                      <p className="text-black text-center">Micheal Jordan</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="flex mr-5 text-black">*****</div>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-light font-light text-black">
+                          Beneden wat dicht bij elkaar maar wij zaten boven en
+                          daar stonden de tafels ver van elkaar. Ongelooflijk
+                          wat het personeel aangaat, zéér vlug en als je het
+                          laatste van je glas wijn had uitgedronken waren ze
+                          daar om het te vullen, deze mensen verdienen 10
+                          sterren.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* REVIEW CARD */}
+              </div>
+            </div>
+            {/* REVIEWS */}
           </div>
         </div>
         {/* DESCRIPTION PORTION */}
