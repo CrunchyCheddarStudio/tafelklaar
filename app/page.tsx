dc6e0c8f920bf3ab7a3f0921669c159dd2efaa58
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2023-04-19 16:08:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,9 +49,47 @@ export default function Home() {
             {/* RESTAURANT NAVBAR */}
             {/* TITLE */}
             <div className="mt-4 border-b pb-6 text-black">
-              <h1 className="font-bold md:text-6xl text-1xl">'T Populierenhof</h1>
+              <h1 className="font-bold md:text-6xl text-1xl">
+                'T Populierenhof
+              </h1>
             </div>
             {/* TITLE */}
+            {/* RATINGS */}
+            <div className="flex items-end">
+              <div className="ratings mt-2 flex items-center">
+                <p className="text-black">*****</p>
+                <p className="text-reg ml-3 text-black">4.9</p>
+              </div>
+              <div>
+                <p className="text-reg ml-4 text-black">600 Recensies</p>
+              </div>
+            </div>
+            {/* RATINGS */}
+            {/* DESCRIPTION */}
+            <div className="mt-4">
+              <p className="text-lg font-light text-black">
+                Restaurant Populierenhof bestaat sinds 1988 en serveert een
+                Franse keuken in een gezellig kader. Laat je inspireren door het
+                vernieuwde interieur & laat je omverblazen door onze verfijnde
+                gerechten. Een rustgevende sfeer van lunch in de week tot diner
+                in het weekend.
+              </p>
+            </div>
+            {/* DESCRIPTION */}
+            {/* IMAGES */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 text-black border-b pb-5">
+                5 Foto's
+              </h1>
+              <div className="flex flex-wrap">
+                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/10/11/73/dd/photo0jpg.jpg" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/0b/d2/d6/dc/photo1jpg.jpg" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/68/t-populierenhof.jpg" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-l/0f/5b/1d/72/koude-visschotel-en-schotel.jpg" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="https://media-cdn.tripadvisor.com/media/photo-s/13/d1/eb/66/t-populierenhof.jpg" alt="" />
+              </div>
+            </div>
+            {/* IMAGES */}
           </div>
         </div>
         {/* DESCRIPTION PORTION */}
