540acd00de422a8bb5d17b3fec4463465594b1c6
Ojin Timo <89027644+OjinTimo@users.noreply.github.com>
2023-04-19 18:23:12 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,26 @@ export default function Home() {
           </div>
           {/* SEARCHBAR */}
         </div>
+        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+          <div className="w-1/5">
+            <div className="border-b pb-4">
+              <h1 className="text-black mb-2">Regio</h1>
+              <p className="font-light text-reg text-black">Hulste</p>
+              <p className="font-light text-reg text-black">Kortrijk</p>
+              <p className="font-light text-reg text-black">Brugge</p>
+              <p className="font-light text-reg text-black">Antwerpen</p>
+              <p className="font-light text-reg text-black">Gent</p>
+              <p className="font-light text-reg text-black">Sint-Niklaas</p>
+              <p className="font-light text-reg text-black">Heule</p>
+            </div>
+            <div className="border-b pb-4 mt-3">
+              <h1 className="text-black mb-2">Keuken</h1>
+              <p className="font-light text-reg text-black">Mexicaans</p>
+              <p className="font-light text-reg text-black">Frans</p>
+              <p className="font-light text-reg text-black">Chinees</p>
+            </div>
+          </div>
+        </div>
         {/* HEADER */}
       </main>
     </main>
